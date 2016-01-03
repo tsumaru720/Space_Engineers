caf669ec0d4dc82ac4a36be3416422ff6d94d7fd
Simon Mott <simon@simonmott.co.uk>
2016-01-03 15:57:07 +0000

    initial import

---
+++ Light Controller.cs
@@ -0,0 +1,90 @@
+﻿using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+using VRageMath;
+using VRage.Game;
+
+
+namespace Skeleton
+{
+    class Light_Cntroller : Skeleton
+    {
+        // The purpose of this script is to allow easy changing of light colour
+        // on your ship or station. This currently only affects interior lights.
+        //
+        // This was originally written so my ship can have Red/Blue alert states
+        // Call this script with argument red/blue to change all lights to red/blue.
+        // You cam then change them to whatever colour you like when you return to normal.
+        //
+        // The only issue with this currently is that if you have different colour lights
+        // in your normal state, then using this script will not keep previous colours.
+        //
+        // USAGE
+        //
+        // Call with either a keyword as an argument (red, blue, yellow)
+        // Call with an RGB value (without spaces) such as 255,255,255
+
+        //Set default value of Light Color to White
+        Color LIGHT_COLOR = new Color(); 
+
+        void Main(string argument)
+        {
+            if (argument.ToLower() == "red")
+            {
+                LIGHT_COLOR = new Color(255, 0, 0);
+            }
+            else if (argument.ToLower() == "blue")
+            {
+                LIGHT_COLOR = new Color(0, 0, 255);
+            }
+            else if (argument.ToLower() == "yellow")
+            {
+                LIGHT_COLOR = new Color(255, 160, 0);
+            }
+            else
+            {
+                string[] RGB = argument.Split(',');
+
+                if (RGB.Length > 1)
+                {
+                    // Multiple elements, probably an RGB value :)
+                    LIGHT_COLOR = new Color(Int32.Parse(RGB[0]), Int32.Parse(RGB[1]), Int32.Parse(RGB[2]));
+                }
+                else
+                {
+                    // Only one element, suggests a word.
+                    // Given We've got this far, suggests its an unknown colour alias
+
+                    // Change lights to default (White) if there is a problem
+                    LIGHT_COLOR = new Color(255, 255, 255);
+                    Echo("Unknown colour alias provided - " + LIGHT_COLOR.ToString());
+                }
+            }
+
+            // We should now have a colour, lets set it!
+            
+            ColorLights(LIGHT_COLOR);
+        }
+
+        void ColorLights(Color NEW_COLOR)
+        {
+            var blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyInteriorLight>(blocks);
+
+            for (var i = 0; i < blocks.Count; i++)
+            {
+                var light = blocks[i] as IMyInteriorLight;
+                if (light.CubeGrid == Me.CubeGrid)
+                {
+                    light.SetValue("Color", NEW_COLOR);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Skeleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI;
+
+namespace Skeleton
+{
+    public abstract class Skeleton : IMyGridProgram
+    {
+        public Action<string> Echo
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public TimeSpan ElapsedTime
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public IMyGridTerminalSystem GridTerminalSystem
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool HasMainMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IMyProgrammableBlock Me
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Storage
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        void IMyGridProgram.Main(string argument)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
