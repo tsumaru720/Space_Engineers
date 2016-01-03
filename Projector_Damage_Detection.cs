4035f5c26048a900cbbab16475563a52b307b771
Simon Mott <simon@simonmott.co.uk>
2016-01-03 17:19:00 +0000

    add buildProgress type, because why not

--- Projector_Damage_Detection.cs
+++ Projector_Damage_Detection.cs
@@ -15,9 +15,16 @@ namespace Skeleton
 
         // Aim of this script is to detect if your ship is missing blocks using a projector
         // In combat, this usually means the block has been destroyed.
-       
+
+        struct buildProgress
+        {
+            public int currentBlocks;
+            public int totalBlocks;
+        }
+
         void Main(string argument)
         {
+
             var blocks = new List<IMyTerminalBlock>();
             GridTerminalSystem.GetBlocksOfType<IMyProjector>(blocks);
 
@@ -31,15 +38,27 @@ namespace Skeleton
 
                     // Abritrarily index 3 is build progress
                     // and 4, if complete, is "Complete!"
+                    // TODO Make this find the index itself, in case the ordering changes.
 
                     // Check if index 3 exists
                     if (projectorDetails.Length >= 3)
                     {
                         // We only care about the progress count
                         string[] buildProgressString = projectorDetails[3].Split(' ');
-                        string[] buildProgress = buildProgressString[2].Split('/');
-                        Echo("Progess: " + buildProgress[0] + " of " + buildProgress[1]);
-                        Echo(Me.CustomName);
+                        string[] buildProgressCounts = buildProgressString[2].Split('/');
+
+                        buildProgress buildProgress = new buildProgress();
+                        buildProgress.currentBlocks = Int32.Parse(buildProgressCounts[0]);
+                        buildProgress.totalBlocks = Int32.Parse(buildProgressCounts[1]);
+
+                        if (buildProgress.currentBlocks < buildProgress.totalBlocks)
+                        {
+                            Echo("ZOMG STUFF IS MISSING");
+                        }
+                        else
+                        {
+                            Echo("Nothing to see here, everything is fine");
+                        }
                     }
                 }
             }
